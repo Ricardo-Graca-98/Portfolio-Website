4bcce7e7a8a725ecc1cf490b9dbbaab633cd4a34
Rose Baker <44160984+thanerose-331995@users.noreply.github.com>
2019-02-26 14:29:53 +0000

    psds

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function myFunction(){
-    document.getElementById("rectangle").setAttribute("width","100");
+var xincrement = 1;
+function scroll(){
+    var top = document.getElementById("cplus").getAttribute("top");
+    document.getElementById("cplus").setAttribute("top", top+1);
 }
